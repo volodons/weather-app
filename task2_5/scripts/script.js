62f5d604b9a2648965d29d9b340ac56c36e5fafa
Volodymyr Donets <volodons@gmail.com>
2023-04-28 11:14:03 +0300

    Create function of initial rendering of current weather

--- task2_5/scripts/script.js
+++ task2_5/scripts/script.js
@@ -1,11 +1,12 @@
+const mainContent = document.getElementById("main-content");
+
 function checkGeolocationSupport() {
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(
-      getGeolocationWeather,
+      getGeolocationWeatherCurrent,
       errorOutput
     );
   } else {
-    const body = document.getElementById("body");
     body.innerHTML = "";
     const errorElement = document.createElement("h1");
     errorElement.innerText =
@@ -14,8 +15,40 @@ function checkGeolocationSupport() {
   }
 }
 
-function getGeolocationWeather(pos) {
-  fetch;
+function getGeolocationWeatherCurrent(pos) {
+  fetch(
+    `https://api.openweathermap.org/data/2.5/weather?lat=${pos.coords.latitude}&lon=${pos.coords.longitude}&units=metric&lang=en&appid=2c635de091adc7b86bb67624d5ba7e05`
+  )
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      console.log(data);
+      console.log(data.name);
+      console.log(data.sys.country);
+      mainContent.innerHTML = `<header class="header">
+          <div class="header header__column">
+            <span class="header__text--big-bold">${Math.round(
+              data.main.temp
+            )}°C</span>
+            <span>Feels like ${Math.round(data.main.feels_like)}°C</span>
+          </div>
+          <div class="header header__column">
+            <span class="header__text--bold">${data.weather[0].main}</span>
+            <span>${data.name}, ${data.sys.country}</span>
+          </div>
+          <div class="header header__column">
+            <img
+              src="https://openweathermap.org/img/wn/${
+                data.weather[0].icon
+              }@2x.png"
+              alt="${data.weather[0].description}"
+              title="${data.weather[0].description}"
+            />
+          </div>
+        </header>`;
+      // body.append(html);
+    });
 }
 
 function errorOutput() {
